ad571aeefb8c6205015cc44ede16066266720f04
Onur Ahmet Yemez <onuryemez.is@gmail.com>
2024-11-29 08:29:09 +0300

    updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const withdraw = document.querySelector("#withdraw");
 const historyBtn = document.querySelector(".btn.history");
 const balance = document.querySelector("#balance");
 const historyContainer = document.querySelector(".history-container");
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 
 depositForm.addEventListener("submit", addBalance);
 withdrawForm.addEventListener("submit", removeBalance);
@@ -15,65 +15,65 @@ let balanceHistory = [];
 
 function addBalance(e) {
   e.preventDefault();
-  const depositInput =Number(deposit.value.trim());
+  const depositInput = Number(deposit.value);
 
-  if(depositInput <= 0) {
-    showAlert('Please enter a positive number','alertDiv')
-  }else {
+  if (depositInput <= 0) {
+    showAlert("Please enter a positive number", "alertDiv");
+  } else {
     balance.textContent = Number(balance.textContent) + depositInput;
     const depositHistory = {
-        Type:'Deposit',
-        Amount: depositInput,
-        Date: new Date().toLocaleString(),
+      Type: "Deposit",
+      Amount: depositInput,
+      Date: new Date().toLocaleString(),
     };
-    balanceHistory = [...balanceHistory,depositHistory];
+    balanceHistory = [...balanceHistory, depositHistory];
   }
-  deposit.value = '';
+  deposit.value = "";
 }
 
 function removeBalance(e) {
-    e.preventDefault();
-    const withdrawInput = Number(withdraw.value.trim());
+  e.preventDefault();
+  const withdrawInput = Number(withdraw.value);
 
-    if(withdrawInput <= 0) {
-        showAlert('Please enter a positive number','alertDiv')
-    } else if (withdrawInput > Number(balance.textContent)) {
-        showAlert('insufficient funds','alertDiv2')
-    } else {
-      balance.textContent = Number(balance.textContent) - withdrawInput;
-      const withdrawHistory = {
-        Type:'Withdraw',
-        Amount: withdrawInput,
-        Date: new Date().toLocaleString(),
+  if (withdrawInput <= 0) {
+    showAlert("Please enter a positive number", "alertDiv");
+  } else if (withdrawInput > Number(balance.textContent)) {
+    showAlert("insufficient funds", "alertDiv2");
+  } else {
+    balance.textContent = Number(balance.textContent) - withdrawInput;
+    const withdrawHistory = {
+      Type: "Withdraw",
+      Amount: withdrawInput,
+      Date: new Date().toLocaleString(),
     };
-    balanceHistory = [...balanceHistory,withdrawHistory];
-    }
-    withdraw.value = '';
+    balanceHistory = [...balanceHistory, withdrawHistory];
+  }
+  withdraw.value = "";
 }
 
-function showAlert(message,className) {
-    const alertDiv = document.createElement('div')
-    alertDiv.classList.add(className);
-    const alertText = document.createTextNode(message);
+function showAlert(message, className) {
+  const alertDiv = document.createElement("div");
+  alertDiv.classList.add(className);
+  const alertText = document.createTextNode(message);
 
-    alertDiv.appendChild(alertText);
-    container.appendChild(alertDiv);
+  alertDiv.appendChild(alertText);
+  container.appendChild(alertDiv);
 
-    setTimeout(() => {
-        alertDiv.remove();
-    },1000)
+  setTimeout(() => {
+    alertDiv.remove();
+  }, 1000);
 }
 
-historyBtn.addEventListener('click',showHistory);
+historyBtn.addEventListener("click", showHistory);
 
 function showHistory() {
-  historyContainer.classList.toggle('active');
+  historyContainer.classList.toggle("active");
 
-  historyContainer.innerHTML = '';
+  historyContainer.innerHTML = "";
 
-  const headerHistory = document.createElement('h2');
-  headerHistory.textContent = 'Account History';
-  headerHistory.classList.add('history-header');
+  const headerHistory = document.createElement("h2");
+  headerHistory.textContent = "Account History";
+  headerHistory.classList.add("history-header");
   historyContainer.appendChild(headerHistory);
 
   balanceHistory.map((balanceItem) => {
